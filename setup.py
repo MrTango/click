23913840b0a53b0650f3d522f5da4ad82203c96e
MrTango2 <md@derico.de>
2018-03-26 23:18:58 +0200

    use static version to use zest.releaser

--- setup.py
+++ setup.py
@@ -6,16 +6,11 @@ from setuptools import setup
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 
-with open('click/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-
 setup(
     name='plonecli-click',
     author='Maik Derstappen',
     author_email='md@derico.de',
-    version=version,
+    version="7.0",
     url='https://github.com/MrTango/click',
     packages=['click'],
     description='A simple wrapper around optparse for '
